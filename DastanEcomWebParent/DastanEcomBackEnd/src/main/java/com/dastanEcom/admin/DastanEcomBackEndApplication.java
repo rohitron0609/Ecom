6eb9506b844a143c8614260281040d3dd57a322b
Rohit Yadav <rohitron0609@gmail.com>
2023-01-01 12:56:08 +0530

    Initial commit

---
+++ DastanEcomWebParent/DastanEcomBackEnd/src/main/java/com/dastanEcom/admin/DastanEcomBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.dastanEcom.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DastanEcomBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DastanEcomBackEndApplication.class, args);
+	}
+
+}
---
+++ DastanEcomWebParent/DastanEcomFrontEnd/src/main/java/com/dastanEcom/site/DastanEcomFrontEndApplication.java
@@ -0,0 +1,13 @@
+package com.dastanEcom.site;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DastanEcomFrontEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DastanEcomFrontEndApplication.class, args);
+	}
+
+}
